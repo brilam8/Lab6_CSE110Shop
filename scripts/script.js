87e2b9f7581904f0c725f1c89c09d64a5a9aad73
Brian Lam <brilam8@gmail.com>
2021-02-09 15:31:53 -0800

    started

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,21 @@
 // Script.js
 
+localStorage = window.localStorage;
+
 window.addEventListener('DOMContentLoaded', () => {
-  // TODO
+  prodList = document.getElementById('product-list')
+  fetch('https://fakestoreapi.com/products')
+    .then(res => res.json())
+    .then(data => {
+      if (!localStorage.getItem('products')) {
+        localStorage.setItem('products', JSON.stringify(data))
+      }
+      else {
+        console.log("already fetched the info and stored it")
+      }
+    })
+  data = JSON.parse(localStorage.getItem('products'))
+  for (prod in data) {
+    prodList.appendChild(document.createElement('product-item'))
+  }
 });
